c18fa312d944aad8a38e234a9e65536cb41f60e6
lekesai <gaogezuishuai1122@gmail.com>
2023-06-13 23:39:26 +0800

    vueG

---
+++ src/views/NextTick.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>NextTick</h2>
+    
+    <div ref="div">{{ message }}</div>
+    <button @click="change">改变</button>
+</template>
+<script>
+    import {ref,nextTick} from 'vue'
+    let message=ref(10)
+    let div=ref()
+    function change(){
+        message.value=20
+        nextTick(()=>{
+            console.log(div.value.innerHTML)
+        })//异步微
+    }
+</script>
+<style>
+</style>
